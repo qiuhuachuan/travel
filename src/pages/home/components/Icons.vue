db0b83b2a84905265014570dc58b40c87bdd7bc8
qiuhuachuan <qhc0327@163.com>
2020-04-21 20:18:03 +0800

    commit 'recommend.vue'

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -102,6 +102,10 @@ export default {
   padding-bottom: 50%;
 }
 
+.icons {
+  margin-top: 0.1rem;
+}
+
 .icon {
   position: relative;
   overflow: hidden;
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-botttom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl"/>
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~@/assets/styles/mixins.styl'
+
+.recommend-title {
+  margin-top: 0.2rem;
+  line-height: .8rem;
+  background: #eee;
+  text-indent: .2rem
+}
+
+.item {
+  display: flex;
+  height: 1.9rem;
+}
+
+.item-img {
+  width: 1.7rem;
+  height: 1.7rem;
+  padding: 0.1rem;
+}
+
+.item-info {
+  flex: 1;
+  padding: 0.1rem;
+  min-width: 0;
+}
+
+.item-title {
+  line-height: 0.54rem;
+  font-size: 0.32rem;
+  ellipsis();
+}
+
+.item-desc {
+  line-height: 0.4rem;
+  color: #ccc;
+  ellipsis();
+}
+
+.item-button {
+  line-height: 0.44rem;
+  height: 0.44rem;
+  background: #ff9300;
+  padding: 0 0.2rem;
+  border-radius: 0.06rem;
+  color: #fff;
+  margin-top: 0.16rem;
+}
+</style>
